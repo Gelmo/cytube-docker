092de0786c8d784d837e3286776f35f42c978878
Unknown <jibgilmon@yahoo.com>
2018-09-27 11:54:02 -0500

    Fix paths

--- scripts/container-install.sh
+++ scripts/container-install.sh
@@ -14,8 +14,8 @@ git clone -b 3.0 https://github.com/calzoneman/sync /home/cytube/app
 mkdir -p /home/cytube/certs
 cd /home/cytube/app
 sed 's/67c7c69a/ffdbce83/' package.json
-cp -f /scripts/config.docker.yaml /home/cytube/app
-cp -f /scripts/run.sh /home/cytube/app
+cp -f /home/cytube/app/scripts/config.docker.yaml /home/cytube/app
+cp -f /home/cytube/app/scripts/run.sh /home/cytube/app
 chown -R cytube /home/cytube
 
 su-exec cytube npm install
