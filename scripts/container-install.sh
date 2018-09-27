ab432891792c25d545f07a69ae64c7a8ea688320
Unknown <jibgilmon@yahoo.com>
2018-09-27 11:28:22 -0500

    Update README & add certs dir

--- scripts/container-install.sh
+++ scripts/container-install.sh
@@ -9,13 +9,14 @@ npm install npm@latest -g
 
 adduser -S cytube
 
-git clone -b 3.0 https://github.com/calzoneman/sync /home/cytube/app
 
+git clone -b 3.0 https://github.com/calzoneman/sync /home/cytube/app
+mkdir -p /home/cytube/certs
 cd /home/cytube/app
 sed 's/67c7c69a/ffdbce83/' package.json
 cp -f /scripts/config.docker.yaml /home/cytube/app
 cp -f /scripts/run.sh /home/cytube/app
-chown -R cytube /home/cytube/app
+chown -R cytube /home/cytube
 
 su-exec cytube npm install
 
