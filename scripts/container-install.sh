d47a4ae7d44a2290ea498771da34f63adb85341d
Unknown <jibgilmon@yahoo.com>
2018-09-29 10:07:10 -0500

    Adjust filters commit

--- scripts/container-install.sh
+++ scripts/container-install.sh
@@ -13,7 +13,7 @@ adduser -S cytube
 git clone -b 3.0 https://github.com/calzoneman/sync /home/cytube/app
 mkdir -p /home/cytube/certs
 cd /home/cytube/app
-sed 's/67c7c69a/ffdbce83/' package.json
+sed 's/67c7c69a/ffdbce8/' package.json
 cp -f /scripts/config.docker.yaml /home/cytube/app
 cp -f /scripts/run.sh /home/cytube/app
 chown -R cytube /home/cytube
